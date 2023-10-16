0808e76c8c997ddfc702e8c7291f7c227fa6fab6
SalvatoreCirasa <uni389305@studium.unict.it>
2023-10-16 22:08:24 +0200

    Initial commit

---
+++ src/main/java/it/Salfrance/SalfranceGamestore/SalfranceGamestoreApplication.java
@@ -0,0 +1,13 @@
+package it.Salfrance.SalfranceGamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalfranceGamestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalfranceGamestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/Salfrance/SalfranceGamestore/SalfranceGamestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package it.Salfrance.SalfranceGamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalfranceGamestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
